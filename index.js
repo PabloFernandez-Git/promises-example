f8cf7f81512e52f5e5e949358b3ca81863142528
PabloFernandez-Git <pablo.ezequiel.fernandez@gmail.com>
2021-11-06 15:00:16 -0300

    Inicio

---
+++ index.js
@@ -0,0 +1,20 @@
+const datos = [
+{
+    id: 1,
+    title: 'Iron Man',
+    year: 2008
+},
+{
+    id: 2,
+    title: 'Spiderman',
+    year: 2017
+},
+{
+    id: 3,
+    title: 'Avengers',
+    year: 2019
+}];
+
+const getDatos = () => {
+    return datos
+}
